7dea0ff0c6c05fd46ca1ed83ae42b4c6c400a159
sandeep patel <spatel@pageupsoft.com>
2024-05-08 19:36:08 +0530

    Added style for excel sheet

--- app/Exports/ExportUser.php
+++ app/Exports/ExportUser.php
@@ -5,10 +5,12 @@ namespace App\Exports;
 use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ExportUser implements FromView
+class ExportUser implements FromView, WithStyles
 {
-    protected $request;
+    protected $request, $users;
 
     public function __construct($request)
     {
@@ -17,8 +19,15 @@ class ExportUser implements FromView
 
     public function view(): View
     {
-        $users = User::select('name','email')->limit(2)->get();
-        return view('user')->with('users', $users);
+        $this->users = User::select('name', 'email')->limit(2)->get();
+        return view('user')->with('users', $this->users);
     }
 
+    public function styles(Worksheet $sheet)
+    {
+        $sheet->getStyle('A1:B' . (count($this->users) + 1))
+            ->getBorders()
+            ->getAllBorders()
+            ->setBorderStyle('thin');
+    }
 }
