dfa87b104976d766ef090d1484b3ca1b828a1e49
sandeep patel <spatel@pageupsoft.com>
2024-05-10 14:10:32 +0530

    Implemented basic @include directive

--- resources/views/renderComponant.blade.php
+++ resources/views/renderComponant.blade.php
@@ -5,17 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>Bootstrap demo</title>
+    <title>Welcome</title>
   </head>
   <body>
     <div class="container">
-        {{-- {{  }} - content must be string here  --}}
-    
-        <h1>Hello, {{ $name }}</h1>
-    
-        @isset($show_time)
-            <h3>Date Time {{ date("Y-m-d H:i:s") }}</h3>
-        @endisset
+       <h1>Welcome</h1><hr>
+       @include('contact', ['address' => 'jabalpur M.P.', 'pincode' => 482004]) {{-- child view  --}} {{-- array contains data , for child view --}}
     </div>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmailController;
 
 Route::get('/send-welcome-email', [EmailController::class, 'sendWelcomeEmail']);
+
+// Passing data to view 
+Route::view('show-user', 'welcome', ['name' => 'Sandeep Patel']);
+
+// Using php()
+Route::view('show-user-with-time', 'welcome',
+['name' => 'Sandeep Patel', 'show_time' => true]);
+
+// componant 
+Route::view('componant', 'renderComponant');   
+
+// passing data to componant - will be availible to it's child view
+Route::view('componant', 'renderComponant', ['contact' => 9302325460]);        
