859067ccb14b977819c55b824dea1017b9fa6220
sandeep patel <spatel@pageupsoft.com>
2024-05-10 16:30:44 +0530

    Implemented @include and @includeIf directive

---
+++ resources/views/products.blade.php
@@ -0,0 +1,10 @@
+<h3>
+    <ul>
+        <li>Samsung</li>
+        <li>Nokia</li>
+        <li>Apple</li>
+        <li>Realme</li>
+        <li>Readme</li>
+    </ul>
+</h3>
+<h4>Total Product Are : {{ $total_product }}</h4>
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmailController;
 
 Route::get('/send-welcome-email', [EmailController::class, 'sendWelcomeEmail']);
+Route::view('/', 'welcome', ["website_name" => 'Click Kart']);
